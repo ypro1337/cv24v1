c35cf18a534782bf19cdd895eb04dd9421019a42
ypro1337 <yahiamok23@gmail.com>
2024-03-27 00:31:15 +0100

    initial commit

---
+++ src/main/java/fr/univrouen/cv24/Cv24v1Application.java
@@ -0,0 +1,14 @@
+package fr.univrouen.cv24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cv24v1Application {
+
+	public static void main(String[] args) {
+		System.getProperties().put("server.port", 8100);
+		SpringApplication.run(Cv24v1Application.class, args);
+	}
+
+}
