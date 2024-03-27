a725f0b4663a029a2862c472173ddd6dd6ae3ffb
ypro1337 <yahiamok23@gmail.com>
2024-03-27 01:39:32 +0100

    fix main ComponentScan issue

--- src/main/java/fr/univrouen/cv24/Cv24v1Application.java
+++ src/main/java/fr/univrouen/cv24/Cv24v1Application.java
@@ -2,8 +2,10 @@ package fr.univrouen.cv24;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"fr.univrouen.cv24v1.controllers"})
 public class Cv24v1Application {
 
 	public static void main(String[] args) {
