a931cd80456caebec5fe7960bb24e73f473eeec1
ypro1337 <yahiamok23@gmail.com>
2024-04-03 07:56:02 +0200

    Version 1

---
+++ src/main/java/fr/univrouen/cv24v1/controllers/GetController.java
@@ -0,0 +1,18 @@
+package fr.univrouen.cv24v1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GetController {
+@GetMapping("/resume")
+public String getListCVinXML() {
+return "Envoi de la liste des CV";
+}
+@GetMapping("/cvid")
+public String getCVinXML(
+@RequestParam(value = "texte") String texte) {
+return ("Détail du CV n°" + texte);
+}
+}
--- src/main/java/fr/univrouen/cv24v1/controllers/IndexController.java
+++ src/main/java/fr/univrouen/cv24v1/controllers/IndexController.java
@@ -1,6 +1,8 @@
 package fr.univrouen.cv24v1.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +10,11 @@ public class IndexController {
 @GetMapping("/")
 public String index() {
 return "Hello cv24 !";
+}
+//adding a basic PostMapping displaying the request body
+@PostMapping("/")
+public String handlePostRequest(@RequestBody String requestBody) {
+    return "Received POST request with body: " + requestBody;
 }
 }
 
